3c2810c993eb92a52736e7a2dfba224984192487
rkociniewski <112274712+rkociniewski@users.noreply.github.com>
2024-06-19 01:48:19 +0200

    Update dependencies

--- src/main/kotlin/org/powermilk/Factorial.kt
+++ src/main/kotlin/org/powermilk/Factorial.kt
@@ -11,7 +11,7 @@ internal const val MESSAGE = "The number should be greater or equal to 0"
  * Function creates string with n! steps.
  *
  * @param number number to calculate n!.
- * @return string with factorial step for 0 to [number].
+ * @return string with a factorial step for 0 to [number].
  */
 fun printFactorial(number: Int) = IntRange(0, number).map { factorial(it).toString() }.joinToString { it }
 
@@ -22,10 +22,12 @@ fun printFactorial(number: Int) = IntRange(0, number).map { factorial(it).toStri
  * @return factorial (n! = [number]!).
  * @exception ArithmeticException when [number] is negative number.
  */
-tailrec fun factorial(number: Int, run: BigInteger = BigInteger.ONE): BigInteger = if (number < 0)
+tailrec fun factorial(number: Int, run: BigInteger = BigInteger.ONE): BigInteger = if (number < 0) {
     throw ArithmeticException(MESSAGE)
-else when (number) {
-    0 -> BigInteger.ZERO
-    1 -> run
-    else -> factorial(number - 1, run.multiply(number.toBigInteger()))
+} else {
+    when (number) {
+        0 -> BigInteger.ZERO
+        1 -> run
+        else -> factorial(number - 1, run.multiply(number.toBigInteger()))
+    }
 }
